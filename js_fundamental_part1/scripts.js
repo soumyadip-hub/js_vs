e06cbc4d8a27c4625a6ae6cbbe6232bde4157415
noobtolearner <soumyadippakhira094@gmail.com>
2025-07-21 16:42:32 +0530

    learn about js fundamentals

---
+++ js_fundamental_part1/scripts.js
@@ -0,0 +1,25 @@
+let js = 'amazing'
+console.log(40 + 8 + 23 - 10);
+
+console.log("jonas");
+console.log(23);
+
+let firstName = "soumya";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "soumya");
+
+let year;
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
